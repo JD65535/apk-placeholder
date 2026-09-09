ae494f26170e0579639b9cecf8d49788461edc32
JD65535 <80062079+JD65535@users.noreply.github.com>
2026-09-09 11:02:18 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.google.android.safetycore"
+    namespace = "com.google.android.verifier.overlay"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.google.android.safetycore"
+        applicationId = "com.google.android.verifier.overlay"
         minSdk = 24
         targetSdk = 34
         versionCode = 2000000000
